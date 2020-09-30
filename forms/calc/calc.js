54e23977490144501e2e75227b942c53004acd77
Emma Campbell <eac46875@creighton.edu>
2020-09-30 16:31:05 -0500

    updates to calc, for, and string

--- forms/calc/calc.js
+++ forms/calc/calc.js
@@ -1,15 +1,20 @@
-let num1 = Number(prompt("Enter in a number"))
-let num2 = Number(prompt("Enter in a second number"))
-alert("hello")
-let numberThree = Number(prompt("Enter in a number"))
-let numberFour = Number(prompt("Enter in a number"))
+/*
+var num1 = Number(prompt("Enter in a number"))
+var num2 = Number(prompt("Enter in a second number"))
+
+function Calculate (numberone, numbertwo) {
+  return numberone + numbertwo;
+  }
+num1.Calculate.call(numberone);
+num2.Calculate.call(numbertwo);
+
+
+let Calculate = (num1 + num2)
 
 
 function Calculate (num1, num2) {
-  
-  
+  return (summedNumbers)
   
   }
 Calculate(numberThree, numberFour)
-let summedNumbers = (num1 + num2)
-alert(summedNumbers)
+*/
