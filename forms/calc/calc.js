e93c4c1d005cc17be305d445fcaa894f3fc68035
Emma Campbell <eac46875@creighton.edu>
2020-09-30 16:07:08 -0500

    hello alert

--- forms/calc/calc.js
+++ forms/calc/calc.js
@@ -1,6 +1,6 @@
 let num1 = Number(prompt("Enter in a number"))
 let num2 = Number(prompt("Enter in a second number"))
-
+alert("hello")
 let numberThree = Number(prompt("Enter in a number"))
 let numberFour = Number(prompt("Enter in a number"))
 
